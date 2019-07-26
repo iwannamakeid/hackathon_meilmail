1225c74c99d5ee02a501af60685f41d4cc278e51
acepilot0620 <49195475+acepilot0620@users.noreply.github.com>
2019-07-26 16:26:10 +0900

    making app account

--- meilmail/meilmail/urls.py
+++ meilmail/meilmail/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import account.views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', account.views.signup, name="signup"),
 ]
